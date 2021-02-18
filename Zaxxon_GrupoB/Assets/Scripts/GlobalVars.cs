2b6839237056af2efbe517c9d41ec6de1a467665
monicavalverdecalvo <monica22parla@gmail.com>
2021-02-18 16:18:38 +0100

    Actualizacion

---
+++ Zaxxon_GrupoB/Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVars : MonoBehaviour
+{
+    public static int energy;
+
+    private void Update()
+    {
+        energy++;
+    }
+
+}
---
+++ Zaxxon_GrupoB/Assets/Scripts/PlaneOffset.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneOffset : MonoBehaviour
+{
+
+    //Componente de tipo Renderer
+    Renderer rend;
+    //Vector de desplazamiento
+    [SerializeField] Vector2 despl;
+    //Datos del juego
+    InitGame initGame;
+    //Velocidad a la que se movera la textura
+    [SerializeField] float scrollSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Asignamos el componente renderer
+        rend = GetComponent<Renderer>();
+        //Obtenemos el script
+        GameObject InitEmpty = GameObject.Find("InitEmpty");
+        initGame = InitEmpty.GetComponent<InitGame>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scrollSpeed = initGame.speed / 2; //Velocidad de desplazamiento
+        //Distancia de desplazamiento, segun el tiempo de transc
+        float offset = Time.time * scrollSpeed;
+        //Vector de desplazamiento
+        despl = new Vector2(0, -offset);
+        //desplazamos la textura albedo y la normal
+        rend.material.SetTextureOffset("_MainTex", despl);
+        rend.material.SetTextureOffset("_BumpMap", despl);
+
+    }
+}
